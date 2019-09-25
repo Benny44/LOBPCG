9a8740c1b9e478ff55a8610a087b1ff25c6b386d
Ben Hermans <ben.hermans2@student.kuleuven.be>
2019-09-25 17:23:08 +0200

    Add matlab documentation for the help call

---
+++ lobpcg_mex.m
@@ -0,0 +1,4 @@
+%LOBPCG_MEX    Compute the smallest eigenvalue of a sparse matrix.
+%   lambda = LOBPCG_MEX(A) produces the smallest eigenvalue of a symmetric,
+% sparse matrix A. If A is dense, use LOBPCG_MEX(sparse(A)).
+
